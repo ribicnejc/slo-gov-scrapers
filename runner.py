b1ab54214857c1963ea9231707c7e6ad6bb58aef
Nejc Ribic <nejc.ribic95@gmail.com>
2019-03-10 15:24:40 +0100

    migrated from python -v 2.7 to 3.7

--- runner.py
+++ runner.py
@@ -2,20 +2,20 @@ from crawlers.gov_spider import SeleniumSpider
 from utils.postgres_handler import DBHandler
 from managers import frontier_manager
 
-import thread
+# import thread
 
 # Here we fill our frontier with first few seeds
 frontier_manager.plant_seeds()
 
 spider1 = SeleniumSpider(frontier_manager.get_next())
-spider2 = SeleniumSpider(frontier_manager.get_next())
-spider3 = SeleniumSpider(frontier_manager.get_next())
+# spider2 = SeleniumSpider(frontier_manager.get_next())
+# spider3 = SeleniumSpider(frontier_manager.get_next())
 
 # while frontier_manager.is_not_empty():
 
 spider1.scrap_page()
-spider2.scrap_page()
-spider3.scrap_page()
+# spider2.scrap_page()
+# spider3.scrap_page()
 
 db = DBHandler()
 db.insert_site()
